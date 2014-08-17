f85b14131e8bd6632832109be9837a6ab6aa9880
sFXplayer <kndungu9@gmail.com>
2014-08-16 21:28:40 -0500

    Basic Forge Stuff

---
+++ src/main/java/com/sFX/RandomUtilitys/RandomUtilitys.java
@@ -0,0 +1,24 @@
+package com.sFX.RandomUtilitys;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "",name ="" ,version = "")
+public class RandomUtilitys {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+    @Mod.EventHandler()
+    public static void Init (FMLInitializationEvent event){
+
+    }
+    @Mod.EventHandler()
+    public static void postInit (FMLPostInitializationEvent event){
+
+    }
+}
